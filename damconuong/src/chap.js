45fea4c66d95ef651e452f59873a6aeb48a67705
Zela <zela08072004@gmail.com>
2026-03-16 15:46:37 +0700

    sửa lại chap.js

--- damconuong/src/chap.js
+++ damconuong/src/chap.js
@@ -1,16 +1,4 @@
-let BASE_URL = 'https://damconuong.plus';
-try { if (CONFIG_URL) BASE_URL = CONFIG_URL; } catch (e) {}
-
-function bypass(url, doc) {
-    // Nhiều trang có cơ chế anti-bot: ban đầu trả HTML chứa script set document.cookie
-    // Nếu phát hiện cookie đó, gọi lại HTTP với header Cookie để qua bước kiểm tra.
-    var cookie = doc.html().match(/document.cookie="(.*?)"/);
-    if (cookie) {
-        cookie = cookie[1];
-        doc = Http.get(url).headers({"Cookie": cookie}).html();
-    }
-    return doc;
-}
+load('config.js');
 
 function execute(url) {
     // Nếu url là tương đối thì nối với BASE_URL
