50e5a0ca6923ab48575a3f9e69a5c9d02f826a4b
Zela <zela08072004@gmail.com>
2026-03-26 22:15:25 +0700

    sua home

--- khotruyen/src/home.js
+++ khotruyen/src/home.js
@@ -1,10 +1,10 @@
 function execute() {
     return Response.success([
-        {title: "Top xếp hạng Qidian", input: "/top-qidian", script: "gen.js"},
-        {title: "Huyền huyễn – Tiên hiệp", input: "/huyen-huyen-tien-hiep", script: "gen.js"},
-        {title: "Độc giả yêu cầu dịch", input: "/yeu-cau-dich", script: "gen.js"},
-        {title: "Đô thị", input: "/do-thi", script: "gen.js"},
-        {title: "Hệ thống", input: "/he-thong", script: "gen.js"},
-        {title: "Khoa học viễn tưởng", input: "/khoa-hoc-vien-tuong", script: "gen.js"},
+        {title: "Top xếp hạng Qidian", input: "https://khotruyenchu.sbs/top-qidian", script: "gen.js"},
+        {title: "Huyền huyễn – Tiên hiệp", input: "https://khotruyenchu.sbs/huyen-huyen-tien-hiep", script: "gen.js"},
+        {title: "Độc giả yêu cầu dịch", input: "https://khotruyenchu.sbs/yeu-cau-dich", script: "gen.js"},
+        {title: "Đô thị", input: "https://khotruyenchu.sbs/do-thi", script: "gen.js"},
+        {title: "Hệ thống", input: "https://khotruyenchu.sbs/he-thong", script: "gen.js"},
+        {title: "Khoa học viễn tưởng", input: "https://khotruyenchu.sbs/khoa-hoc-vien-tuong", script: "gen.js"},
     ]);
 }
