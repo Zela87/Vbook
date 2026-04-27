64e5c91d51e957443c494e05c9b30d7225ffd1d5
Zela <zela08072004@gmail.com>
2026-04-28 01:27:32 +0700

    fix home

--- htvnxx/src/home.js
+++ htvnxx/src/home.js
@@ -2,10 +2,10 @@ load("config.js");
 
 function execute() {
     return Response.success([
-        { title: "Mới cập nhật", input: "/truyen-hentai/", script: "gen.js" },
-        { title: "Trending", input: "/truyen-hentai/?m_orderby=trending", script: "gen.js" },
-        { title: "Xếp hạng", input: "/truyen-hentai/?m_orderby=rating", script: "gen.js" },
-        { title: "Xem nhiều", input: "/truyen-hentai/?m_orderby=views", script: "gen.js" },
-        { title: "Truyện mới", input: "/truyen-hentai/?m_orderby=new-manga", script: "gen.js" }
+        { title: "Mới cập nhật", input: BASE_URL + "/truyen-hentai/", script: "gen.js" },
+        { title: "Trending", input: BASE_URL + "/truyen-hentai/?m_orderby=trending", script: "gen.js" },
+        { title: "Xếp hạng", input: BASE_URL + "/truyen-hentai/?m_orderby=rating", script: "gen.js" },
+        { title: "Xem nhiều", input: BASE_URL + "/truyen-hentai/?m_orderby=views", script: "gen.js" },
+        { title: "Truyện mới", input: BASE_URL + "/truyen-hentai/?m_orderby=new-manga", script: "gen.js" }
     ]);
 }
