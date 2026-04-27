28aeb1f695ea6e1078440137fcddd647c75d771c
Zela <zela08072004@gmail.com>
2026-04-28 01:32:35 +0700

    remove htvn sau khi rename

--- htvn/src/home.js
+++
@@ -1,8 +0,0 @@
-load('config.js');
-function execute() {
-    return Response.success([
-        {title: "Trending", input: "/?m_orderby=trending", script: "gen.js"},
-        {title: "Xếp Hạng", input: "/?m_orderby=rating", script: "gen.js"},
-        {title: "Xem Nhiều", input: "//?m_orderby=views", script: "gen.js"}
-    ]);
-}
