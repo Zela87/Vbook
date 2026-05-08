8667dfbe7eaceb3e5e00084571af3f74762caf11
Zela <zela08072004@gmail.com>
2026-05-08 13:25:06 +0700

    delete

--- sstruyen-zela/src/gen.js
+++ sstruyen-zela/src/gen.js
@@ -1,21 +1,21 @@
-load ('config.js');
+load('config.js');
 load('utils.js');
 
 function execute(url, page) {
     if (!page) page = '1';
-    
+
     let fetchUrl = url;
     // Nếu website hỗ trợ query param hoặc bạn muốn giữ logic URL cũ:
     if (page !== '1') {
         if (fetchUrl.endsWith('/')) fetchUrl = fetchUrl.slice(0, -1);
-        fetchUrl += "?page=" + page; 
+        fetchUrl += "?page=" + page;
     }
 
     let response = fetch(fetchUrl);
     if (response.ok) {
         let doc = response.html();
         let novelList = parseNovelList(doc);
-        
+
         let next = "";
         let nextEl = doc.select("div.paging a:has(span:contains(›)), div.phan-trang a:contains(❭)").first();
         if (nextEl) {
--- sstruyen-zela/src/genre.js
+++ sstruyen-zela/src/genre.js
@@ -1,11 +1,11 @@
-load ('config.js');
+load('config.js');
 
 function execute() {
     let response = fetch(BASE_URL);
     if (response.ok) {
         let doc = response.html();
         let genres = [];
-        doc.select("a[href*='/the-loai/']").forEach(function(el) {
+        doc.select("a[href*='/the-loai/']").forEach(function (el) {
             let title = el.text().trim();
             if (title && title.length < 30) {
                 genres.push({
